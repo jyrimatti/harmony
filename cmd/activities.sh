4f66a29aa9a9acf2ade65414e488a8076cbb5d2d
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-01-15 22:04:12 +0200

    update nix

--- cmd/activities.sh
+++ cmd/activities.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure -i dash -I channel:nixos-23.05-small -p nix dash jq
+#! nix-shell --pure -i dash -I channel:nixos-23.11-small -p nix dash jq
 set -eu
 
 ./query.sh show_config | jq -r '.Activities | .[]'
--- query.sh
+++ query.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure -i dash -I channel:nixos-23.05-small -p dash gnugrep gnused python3Packages.aioharmony
+#! nix-shell --pure -i dash -I channel:nixos-23.11-small -p dash gnugrep gnused python3Packages.aioharmony
 set -eu
 
 command="$*"
