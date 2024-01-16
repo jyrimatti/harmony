fa2d750ba71f441d303eee218a6583fe60ad3591
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-01-16 17:35:44 +0200

    try fix param passing

--- query.sh
+++ query.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env nix-shell
 #! nix-shell --pure -i dash -I channel:nixos-23.11-small -p dash gnugrep gnused python3Packages.aioharmony
-set -e
+set -eu
 
-aioharmony --harmony_ip "$(cat .harmony-ip)" "$1" "$2" "$3" "$4" "$5" \
+aioharmony --harmony_ip "$(cat .harmony-ip)" "$1" ${2:+"$2"} ${3:+"$3"} ${4:+"$4"} ${5:+"$5"} \
   | grep -v '^Trying to connect to HUB'\
   | grep -v '^Connected to HUB Harmony Hub'\
   | grep -v '^HUB: Harmony Hub$'\
